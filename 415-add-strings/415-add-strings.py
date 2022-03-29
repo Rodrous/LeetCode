3a107a55f55776d97d206855cf280115cf47f164
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-03-29 08:12:32 +0530

    Time: 544 ms (5.01%), Space: 14.1 MB (48.12%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def conversion(self,num:str) -> int:
+        value = 0
+        for i,j in enumerate(num[::-1]):
+            value+=(ord(j)-ord("0"))*10**i
+        return value
+    
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1 = self.conversion(num1)
+        num2 = self.conversion(num2)
+        
+        return str(num1+num2)
