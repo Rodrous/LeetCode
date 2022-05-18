483a9ac7427569d7ed6b1b613aa767ecf83e36f5
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-05-19 00:56:39 +0530

    Time: 81 ms (42.50%), Space: 18.6 MB (8.49%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.memoryMap = {}
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        self.fillMemoryMap(root)
+        maxCount: int = max(self.memoryMap.values())
+        result: List = []
+        
+        for key,value in self.memoryMap.items():
+            if value == maxCount:
+                result.append(key)
+        return result
+    
+    
+    def fillMemoryMap(self,root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            if root.val not in self.memoryMap:
+                self.memoryMap[root.val] = 1
+            elif root.val in self.memoryMap:
+                self.memoryMap[root.val] +=1
+            
+            self.fillMemoryMap(root.left)
+            self.fillMemoryMap(root.right)
+        
