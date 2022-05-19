ee1f4ba3e4c25356557de1f3d5beb90b6de0c942
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-05-19 11:09:37 +0530

    Time: 104 ms (58.25%), Space: 15.5 MB (51.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,25 +1,33 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r  = 0, len(nums)-1
-        lpos, rpos = -1, -1
-        while l <= r:
-		    # To Handle Overflow
-            m = l + (r-l)//2
-            if nums[m] == target:
-                lpos = m
-                r = m - 1
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
-        l, r  = 0, len(nums)-1        
-        while l <= r:
-            m = l + (r-l)//2
-            if nums[m] == target:
-                rpos = m
-                l = m + 1
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
-        return [lpos, rpos]
+class Solution(object):
+    def searchRange(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        foundTarget = False
+        breakP = 0
+        firstP = 0
+        lastP = 0
+        while l<=r :
+            mid = (l+r)//2
+            if nums[mid]==target :
+                if not breakP :
+                    breakP = mid
+                    foundTarget = True
+                l=mid+1
+                lastP = mid
+            elif nums[mid] > target :
+                r= mid-1
+            else :
+                l=mid+1
+        if foundTarget :
+            r = breakP
+            l = 0
+            while l<=r:
+                mid = (l+r)//2
+                if nums[mid]==target :
+                    r=mid-1
+                    firstP = mid
+                else :
+                    l=mid+1
+        if not foundTarget :
+            return [-1,-1]
+        return [firstP,lastP]
