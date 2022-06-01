eb26386b29f48d38fb1d95a2e935e7e789507d7a
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-06-02 00:11:48 +0530

    Time: 62 ms (8.51%), Space: 14 MB (29.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        resultant: List = []
+        result: List = []
         digitMap: Dict = {
-            "2" : ["a","b","c"],
-            "3" : ["d","e","f"],
-            "4" : ["g","h","i"],
-            "5" : ["j","k","l"],
-            "6" : ["m","n","o"],
-            "7" : ["p","q","r","s"],
-            "8" : ["t","u","v"],
-            "9" : ["w","x","y","z"]
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
-            
         
-        def backTrack(i: int, curStr: str):
-            if len(curStr) == len(digits):
-                resultant.append(curStr)
-                return
+        def backTrack(i: int, solStr: str) -> None:
+            if len(solStr) == len(digits):
+                result.append(solStr)
+                return 
             
-            for c in digitMap[digits[i]]:
-                backTrack(i+1,curStr + c)
+            for char in digitMap[digits[i]]:
+                backTrack(i+1, solStr+char)
+        
         if digits:
             backTrack(0,"")
         
-        return resultant
-        
+        return result
