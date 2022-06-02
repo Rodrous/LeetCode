7da1da7f7c274c0d64ce07d33b7cb7564a1447f8
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-06-02 23:53:13 +0530

    Time: 71 ms (28.13%), Space: 13.9 MB (76.12%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,29 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        #find a pivot point
+        pivot: int = 0
+        
+        for i in range(len(nums)-1,0,-1):
+            if nums[i-1] < nums[i]:
+                pivot = i
+                break
+        
+        if pivot == 0:
+            nums.sort()
+            return 
+        
+        #Find the first number > pivot
+        swap:int = len(nums)-1
+        
+        while nums[pivot-1] >= nums[swap]:
+            swap -=1
+        
+        #swap
+        
+        nums[swap],nums[pivot-1] = nums[pivot-1] , nums[swap]
+        
+        nums[pivot:] = reversed(nums[pivot:])
