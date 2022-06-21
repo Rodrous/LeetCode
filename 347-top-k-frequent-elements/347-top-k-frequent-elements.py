16f4e09f14a41722c566908eb29af4d0549ff936
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-06-21 13:44:51 +0530

    Time: 110 ms (89.22%), Space: 18.7 MB (70.09%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,6 +1,15 @@
+import operator
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-         return [i[0] for i in Counter(nums).most_common(k)]
         
-       
+        memoryDict: Dict = {}
+        for i in nums:
+            if i in memoryDict:
+                memoryDict[i] += 1
+            else:
+                memoryDict[i] = 1
+
+        
+        sorted_freq = dict(sorted(memoryDict.items(), key=operator.itemgetter(1),reverse=True))
+        return(list(sorted_freq.keys())[0:k])
         
