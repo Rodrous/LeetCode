b6698d5fbd74e890a3ba472ec34bfe67fbaf4394
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-06-23 00:01:10 +0530

    Time: 31 ms (97.04%), Space: 13.9 MB (93.67%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSame(self,p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    def isSameTree(self,p,q):
         if not p and not q:
             return True
         if not p or not q:
@@ -13,7 +13,6 @@ class Solution:
         if p.val != q.val:
             return False
         
-        return self.isSame(p.left,q.right) and self.isSame(p.right, q.left)
-    
+        return self.isSameTree(p.left, q.right) and self.isSameTree(p.right,q.left)
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isSame(root.left,root.right)
+        return self.isSameTree(root.left, root.right)
