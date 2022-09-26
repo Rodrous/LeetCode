39b5030f2f572205eac4609bb60961511c9b1fcd
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-09-26 08:03:32 +0530

    Time: 173 ms (5.24%), Space: 16.3 MB (8.93%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,9 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @lru_cache()
+        memory = {}
         def dfs(n):
             if n >= len(cost):
                 return 0
-            return cost[n] + min(dfs(n+1), dfs(n+2))
+            if n in memory:
+                return memory[n]
+            else:
+                memory[n] = cost[n] + min(dfs(n+1), dfs(n+2))
+            return memory[n]
         
         return min(dfs(0),dfs(1))
